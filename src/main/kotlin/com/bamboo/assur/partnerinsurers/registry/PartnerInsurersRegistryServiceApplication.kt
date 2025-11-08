4a78c1d72b14b3bd892776d5316ff8df69da42cd
Mel Sardes <100629918+MelSardes@users.noreply.github.com>
2025-11-08 20:34:19 +0100

    ♻️ refactor: update the main application class name to match the new naming convention
    Signed-off-by: Mel Sardes <100629918+MelSardes@users.noreply.github.com>

--- src/main/kotlin/com/bamboo/assur/partnerinsurers/registry/PartnerInsurersRegistryServiceApplication.kt
+++ src/main/kotlin/com/bamboo/assur/partnerinsurers/registry/PartnerInsurersRegistryServiceApplication.kt
@@ -2,13 +2,11 @@ package com.bamboo.assur.partnerinsurers.registry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class PartnerInsurersServiceApplication
+class PartnerInsurersRegistryServiceApplication
 
 @Suppress("SpreadOperator")
 fun main(args: Array<String>) {
-    runApplication<PartnerInsurersServiceApplication>(*args)
+    runApplication<PartnerInsurersRegistryServiceApplication>(*args)
 }
