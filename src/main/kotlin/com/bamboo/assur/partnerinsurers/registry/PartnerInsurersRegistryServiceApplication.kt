8f3965b063d5b422f72a977cbf2cec6ba6837d78
Mel Sardes <100629918+MelSardes@users.noreply.github.com>
2025-11-09 11:19:17 +0100

    🧱 build: import shared kernel exception handling beans
    Signed-off-by: Mel Sardes <100629918+MelSardes@users.noreply.github.com>

--- src/main/kotlin/com/bamboo/assur/partnerinsurers/registry/PartnerInsurersRegistryServiceApplication.kt
+++ src/main/kotlin/com/bamboo/assur/partnerinsurers/registry/PartnerInsurersRegistryServiceApplication.kt
@@ -1,9 +1,13 @@
 package com.bamboo.assur.partnerinsurers.registry
 
+import com.bamboo.assur.partnerinsurers.sharedkernel.presentation.ApiResponseBodyAdvice
+import com.bamboo.assur.partnerinsurers.sharedkernel.presentation.GlobalExceptionHandler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(ApiResponseBodyAdvice::class, GlobalExceptionHandler::class)
 class PartnerInsurersRegistryServiceApplication
 
 @Suppress("SpreadOperator")
